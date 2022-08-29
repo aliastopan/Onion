0fa7a646788c38a7e9afe3aaa34fef1c2738e45e
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-30 05:16:40 +0700

    add Swagger support

---
+++ src/Api/Routes/Common/SwaggerEndpoint.cs
@@ -0,0 +1,21 @@
+namespace Onion.Api.Routes.Common;
+
+public class SwaggerEndpoint : IRouteEndpoint, IRouteService
+{
+    public void DefineEndpoints(WebApplication app)
+    {
+        if(app.Environment.IsProduction())
+        {
+            return;
+        }
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    public void DefineServices(IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+}
