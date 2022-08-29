c163526f51845f75c023651823d671c2ce68bdfe
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-30 05:11:40 +0700

    configure logging

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -2,6 +2,18 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.ConfigureLogging((context, logging) =>
+{
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(context.Configuration)
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .CreateLogger();
+
+    logging.ClearProviders();
+    logging.AddSerilog();
+});
+
 builder.Host.ConfigureServices((_, services) =>
 {
     services.AddRouteEndpoints(Assembly.GetExecutingAssembly());
--- src/Api/Usings.cs
+++ src/Api/Usings.cs
@@ -1,2 +1,3 @@
 global using Onion.Api.Extensions;
 global using Onion.Api.Interfaces;
+global using Serilog;
