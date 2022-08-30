ccf770d4bbdb0ce552ca7e9035800e99459328b0
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-31 02:42:56 +0700

    feat: user registration

--- src/Application/Identity/Commands/Registration/RegisterCommandHandler.cs
+++ src/Application/Identity/Commands/Registration/RegisterCommandHandler.cs
@@ -1,8 +1,21 @@
+using Onion.Domain.Entities.Identity;
+
 namespace Onion.Application.Identity.Commands.Registration;
 
 public class RegisterCommandHandler
     : IRequestHandler<RegisterCommand, Result<RegisterCommandResponse>>
 {
+    private readonly IDbContext _dbContext;
+    private readonly ISecureHash _secureHash;
+
+    public RegisterCommandHandler(
+        IDbContext dbContext,
+        ISecureHash secureHash)
+    {
+        _dbContext = dbContext;
+        _secureHash = secureHash;
+    }
+
     public Task<Result<RegisterCommandResponse>> Handle(RegisterCommand request,
         CancellationToken cancellationToken)
     {
@@ -14,8 +27,42 @@ public class RegisterCommandHandler
             return Task.FromResult(result);
         }
 
-        var response = new RegisterCommandResponse(Guid.NewGuid(), request.Username);
+        var validation = ValidateEntry(request.Username, request.Email);
+        if(!validation.IsSuccess)
+        {
+            result = Result<RegisterCommandResponse>.Conflict(validation.Errors.ToArray());
+            return Task.FromResult(result);
+        }
+
+        var user = CreateUser(request.Username, request.Email, request.Password);
+        var response = new RegisterCommandResponse(user.Id, user.Username);
         result = Result<RegisterCommandResponse>.Ok(response);
         return Task.FromResult(result);
+    }
+
+    private Result ValidateEntry(string username, string email)
+    {
+        User user = _dbContext.Users.Search(username)!;
+        if(user is not null)
+        {
+            return Result.Conflict();
+        }
+
+        user = _dbContext.Users.SearchByEmail(email)!;
+        if(user is not null)
+        {
+            return Result.Conflict();
+        }
+
+        return Result.Ok();
+    }
+
+    private User CreateUser(string username, string email, string password)
+    {
+        var hash = _secureHash.HashPassword(password, out string salt);
+        var user = new User(username, email, hash, salt);
+        _dbContext.Users.Add(user);
+        _dbContext.Commit();
+        return user;
     }
 }
