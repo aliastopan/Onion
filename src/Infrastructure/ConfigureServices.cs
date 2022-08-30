27445d44e6cb0b5c82d9b05933e05f441ab8fa41
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-30 23:30:05 +0700

    add in-memory database

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -1,5 +1,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Onion.Application.Common.Interfaces;
+using Onion.Infrastructure.Extensions;
+using Onion.Infrastructure.Persistence;
 
 namespace Onion.Infrastructure;
 
@@ -8,6 +12,24 @@ public static class ConfigureServices
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
         IConfiguration configuration)
     {
+        services.AddInfrastructureDbContext(configuration);
+        return services;
+    }
+
+    internal static IServiceCollection AddInfrastructureDbContext(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        if(configuration.UseInMemoryDatabase())
+        {
+            services.AddDbContext<IDbContext, ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase(nameof(ApplicationDbContext));
+            });
+        }
+        else
+        {
+            throw new NotImplementedException();
+        }
         return services;
     }
 }
