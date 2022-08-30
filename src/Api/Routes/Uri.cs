d67df8a64c803f3bac832716c1bf081428431e00
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-30 23:42:48 +0700

    add static uri reference

---
+++ src/Api/Routes/Uri.cs
@@ -0,0 +1,11 @@
+namespace Onion.Api.Routes;
+
+public static class Uri
+{
+    public static class Identity
+    {
+        public const string Tag = nameof(Identity);
+        public const string Register = "/api/register";
+        public const string Login = "/api/login";
+    }
+}
