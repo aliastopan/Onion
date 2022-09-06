30b321f771d8503a058f080426db6c4c10c5297d
Taufan Augusta <taufan.augusta@protonmail.com>
2022-09-06 20:07:21 +0700

    add global usings

--- src/Api/Usings.cs
+++ src/Api/Usings.cs
@@ -3,4 +3,5 @@ global using MediatR;
 global using Microsoft.AspNetCore.Mvc;
 global using Onion.Api.Extensions;
 global using Onion.Api.Interfaces;
+global using Onion.Api.Mapping;
 global using Serilog;
